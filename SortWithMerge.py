891666f8b2e66ddc1670b5d5a1f0151f35417fc3
Nandannambiar191 <nambiarnandan@outlook.com>
2026-08-31 06:56:42 -0500

    Add files via upload

---
+++ SortWithMerge.py
@@ -0,0 +1,23 @@
+def merge_sort(arr):
+    if len(arr) <= 1:
+        return arr
+    
+    mid = len(arr) // 2
+    left = merge_sort(arr[:mid])
+    right = merge_sort(arr[mid:])
+    return merge(left, right)
+
+def merge(left, right):
+    result = []
+    while left and right:
+        if left[0] < right[0]:
+            result.append(left.pop(0))
+        else:
+            result.append(right.pop(0))
+    while left:
+        result.append(left.pop(0))
+    while right:
+        result.append(right.pop(0))
+    return result
+arr = [8, 3, 5, 2, 7, 1]
+print(merge_sort(arr))
